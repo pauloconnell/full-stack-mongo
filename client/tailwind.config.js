2f515697d4ef18f8f7adfcfaec6e76ed92141538
Paul O'Connell <p_ollie@hotmail.com>
2025-11-20 21:14:57 -0800

    UPdate: fix Tailwind V4 to v3

---
+++ client/tailwind.config.js
@@ -0,0 +1,11 @@
+export default {
+  darkMode: 'class', // or 'media' if you want auto based on system
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
